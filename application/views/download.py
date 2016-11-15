047aa2a1dc405c8419d7d2e57933cb413d67b67a
nurfitriyanto <nurfitriyanto13@gmail.com>
2016-11-15 14:32:18 +0700

    clean direct download link

--- application/views/download.py
+++ application/views/download.py
@@ -10,25 +10,31 @@ from application import app
 from config.general import CLIENT_ID
 from flask import request, url_for, redirect, jsonify
 
-
 @app.route('/api/v1_2/download')
 def download():
-    id_file     = request.args.get('id')
-    url         = 'http://api.soundcloud.com/tracks/%s/streams?client_id=%s' % (id_file, CLIENT_ID)
-    req         = requests.get(url)
-    results     = json.loads(req.text)
+    try:
+        id_file     = request.args.get('id')
+        url         = 'http://api.soundcloud.com/tracks/%s/streams?client_id=%s' % (id_file, CLIENT_ID)
+        req         = requests.get(url)
+        results     = json.loads(req.text)
 
-    rows = {}
-    if results:
-        rows['meta'] = {
-            'code'      : 200,
-            'message'   : 'OK'
-        }
-        rows['data'] = {
-            'url_file'      : results['http_mp3_128_url'],
-            'url_preview'   : results['preview_mp3_128_url']
-        }
-    else:
+        rows = {}
+        if results:
+            rows['meta'] = {
+                'code'      : 200,
+                'message'   : 'OK'
+            }
+            rows['data'] = {
+                'url_file'      : results['http_mp3_128_url'].replace('\u0026', '&'),
+                'url_preview'   : results['preview_mp3_128_url']
+            }
+        else:
+            rows['meta'] = {
+                'code'      : 404,
+                'message'   : 'error : data not found'
+            }
+            rows['data'] = 0
+    except:
         rows['meta'] = {
             'code'      : 500,
             'message'   : 'error 500'
